fda7ba39bbf4f1b87b2ab8a457bedc2edff5fe92
Barabash-Vlad <65270387+Barabash-Vlad@users.noreply.github.com>
2020-06-06 07:04:27 +0300

    added_dal pj

---
+++ folder/adding_situation/adding_situation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace adding_situation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
