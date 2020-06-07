e482234dd4527d064db1167ff7286aeb2ffdd89b
Barabash-Vlad <65270387+Barabash-Vlad@users.noreply.github.com>
2020-06-08 02:59:30 +0300

    added DTO

---
+++ folder/adding_situation/BILL/DTO/Situation_DTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BILL.DTO
+{
+    class Situation_DTO
+    {
+        public int id_situation { get; set; }
+        public string situation_type { get; set; }
+        public string situation_fullname { get; set; }
+        public DateTime situation_datetime { get; set; }
+    }
+}
