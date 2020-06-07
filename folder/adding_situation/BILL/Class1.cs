cb5fde4911417b04fda6a69c2dbd1e9cdeccde7b
Barabash-Vlad <65270387+Barabash-Vlad@users.noreply.github.com>
2020-06-08 02:39:09 +0300

    added and filled in ident, securitycontext

--- folder/adding_situation/BILL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BILL
-{
-    public class Class1
-    {
-    }
-}
---
+++ folder/adding_situation/CLL/Security/Identity/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLL.Security.Identity
+{
+    public abstract class User
+    {
+        public User(int userId, string name, string login, string password)
+        {
+            UserID = userId;   
+            Name = name;
+            Login = login;
+            Password = password;
+        }
+        public int UserID { get; }
+        public string Name { get; }
+        protected string Login { get; }
+        public string Password { get; }
+    }
+}
+
